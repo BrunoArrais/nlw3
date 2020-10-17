b4e9a067bbb1ef37990a39d971f2633ce064616c
bruno arrais <brunoarraisbarretos@gmail.com>
2020-10-17 14:31:38 -0300

    aula 2

---
+++ public/scripts/page-orphanage.js
@@ -0,0 +1,54 @@
+const options = {
+    dragging: false,
+    touchZoom: false,
+    doubleClickZoom: false,
+    scrollWheelZoom: false,
+    zoomControl: false,
+}
+
+//Create Map
+const map = L.map('mapid', options).setView([-27.222633, -49.6455874], 15);
+
+
+//Create and add titleLayer
+L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29,68],
+    popupAnchor: [170,2]
+})
+
+
+
+//Create icon
+L
+.marker([-27.222633, -49.6455874],{ icon })
+.addTo(map)
+
+// image gallery
+
+function selectImage(event) {
+    const button = event.currentTarget
+
+    //remover todas as classes .active
+    const buttons = document.querySelectorAll(".images buttons")
+    buttons.forEach(removeActiveClass)
+
+    function removeActiveClass(button) {
+        button.classList.remove("active")
+    }
+    
+    //selecionar a image clicada
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+    //atualizar o container de imagem
+    imageContainer.src = image.src
+    //adicionar a classe . active para este botao
+
+    button.classList.add('active')
+
+}
